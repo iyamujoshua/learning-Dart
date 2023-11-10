d4485d33c9dd190640100d5688068f81142457b8
iyamujoshua <joshamu20@gmail.com>
2023-11-10 16:49:30 +0100

    learning dart

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  // var age = 25;
+  // print(age.runtimeType);
+
+  // var list = ["appple", "banana", "orange"];
+  // print(list);
+  bool isRaning = true;
+  print(isRaning);
+}
