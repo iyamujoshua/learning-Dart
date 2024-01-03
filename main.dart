e026afa0f2234edcc403d06f553bfbd2c04e008d
iyamujoshua <joshamu20@gmail.com>
2024-01-03 20:29:22 +0100

    back to my learning js

--- main.dart
+++ main.dart
@@ -1,11 +1,108 @@
 import 'dart:io';
+import 'dart:mirrors';
 
-void main() {
-  // var age = 25;
-  // print(age.runtimeType);
+// void main() {
+// stdout.write("what is your name");
+// var name = stdin.readLineSync();
+// print("my name is $name");
+// DAY 1
+// var age = 25;
+// print(age.runtimeType);
 
-  // var list = ["appple", "banana", "orange"];
-  // print(list);
-  bool isRaning = true;
-  print(isRaning);
+// var list = ["appple", "banana", "orange"];
+// print(list);
+
+// bool isRaning = true;
+// print(isRaning.runtimeType);
+
+// int number1 = 15;
+// int number2 = 2;
+// double total = number1 / number2;
+// print(total.runtimeType);
+
+// var iyamuaise = {"alarm": "start learning dart", "year":2023};
+// print(iyamuaise.runtimeType);
+// String strNumber = "42";
+// int intNumber = int.parse(strNumber);
+
+// print(intNumber.runtimeType);
+
+// String doubleNumber = "1.1";
+// double doubleNumber2 = double.parse(doubleNumber);
+// print(doubleNumber2.runtimeType);
+
+// int salary = 2000;
+// String income = salary.toString();
+// print(income.runtimeType);
+
+// double odds = 2.25686402222;
+// String myOdds = odds.toStringAsFixed(3);
+// print(myOdds.runtimeType);
+// String oneAsString = 1.toString();
+
+// Day2
+// const age = 20;
+// const futureAge = 50;
+// const presentAge = futureAge - age;
+// print(presentAge);
+// if (presentAge <= 20) {
+//   print("this age is correct");
+// } else {
+//   print("this is not correct");
+// }
+
+// stdout.write("welcome tell us your name");
+// var name = stdin.readLineSync();
+// print("nice meeting you $name");
+
+// const numz = 100;
+// const times = numz * 2;
+// print(times);
+// }
+
+// class Num {
+//   int num = 10;
+// }
+
+// main() {
+//   var n = Num();
+//   int number;
+//   if (n != null) {
+//     number = n.num;
+//     print(number);
+//   }
+// }
+
+class Num {
+  int num = 100;
+}
+
+main() {
+  // var n = Num();
+  // int number;
+  // if (n != null) {
+  //   number = n.num;
+  //   print(number);
+  // }
+
+  // var xnum;
+  // var xTotal = xnum ?? 5;
+  // print(xTotal);
+
+  // int x = 101;
+  // var result = x % 2 == 0 ? "Even" : "Odd";
+  // print(result);
+
+  // String y = "joshua";
+  // if (y is int) {
+  //   print("integer");
+  // } else {
+  //   print("try again");
+  // }
+
+  // standard for loop
+  // for (var i =1; i <= 10; ++i) {
+  //   print(i);
+  // }
+
 }
---
+++ while_loop.dart
@@ -0,0 +1,11 @@
+void main() {
+  // var num = 5;
+  // while (num > 0) {
+  //   print(num);
+  //   num -= 1;
+  // }
+
+  var open1 = 10;
+  var open2 = 7;
+  print(open1 > open2);
+}
