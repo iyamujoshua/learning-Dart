b6e0ee0eaa94b2e04580920bbc1d144b30f8fbbd
iyamujoshua <joshamu20@gmail.com>
2024-01-05 21:48:14 +0100

    datatypes, list and maps

--- while_loop.dart
+++ while_loop.dart
@@ -2,6 +2,39 @@ import 'dart:io';
 
 main() {
   stdout.write("what is your name ");
-  var name = stdin.readLineSync();
-  print("my name is $name");
+  var output = stdin.readLineSync();
+  print("my name is $output");
+
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+  print(name);
+  print(year);
+  print(antennaDiameter);
+  print(flybyObjects);
+  print(image);
+
+  if (2 is String) {
+    print("correct");
+  } else {
+    print("wrong");
+  }
+
+  String fullName = "Joshua";
+  int age = 20;
+  bool male = true;
+  List<String> fruits = ["mango", "apple"];
+  Map<String, dynamic> myData = {
+    "name": fullName,
+    "age": age,
+    "sex": male,
+    "food": fruits,
+  };
+  print(myData);
+  //undertanding control flow statement through if and else
 }
